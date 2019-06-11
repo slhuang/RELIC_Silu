c6c9eb33c502ecbd160d1e039825ff9922bc4ea1
Silu Huang <silu@Silus-MacBook-Pro.local>
2019-06-12 00:44:54 +0800

    update

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def clear_files(dir='.result/'):
     cmd = 'rm ' + dir + '*.csv'
     os.system(cmd)
 
-def preserving_ops(dir_path, endwith, result_dir):
+def preserving_ops(dir_path, endwith, result_dir, with_pk, approx=0):
     files=sorted(os.listdir(dir_path))
     print (files)
     artifacts = ''
@@ -33,7 +33,13 @@ def preserving_ops(dir_path, endwith, result_dir):
         if f.endswith(endwith): #f.startswith('repo'): #
             artifacts += dir_path + f + ','
     print ("----------preserving-ops----------")
-    proc = subprocess.Popen(['./src/preserving_ops/preserving_ops', '-dir', artifacts, '-result', result_dir], stdout=subprocess.PIPE)
+    if approx != 0:
+        proc = subprocess.Popen(['./src/preserving_ops/preserving_ops', '-dir', artifacts, '-result', result_dir, '-approx', str(approx)], stdout=subprocess.PIPE)
+    else:
+        if with_pk:
+            proc = subprocess.Popen(['./src/preserving_ops/preserving_ops', '-dir', artifacts, '-result', result_dir], stdout=subprocess.PIPE)
+        else:
+            proc = subprocess.Popen(['./src/preserving_ops/preserving_ops', '-dir', artifacts, '-result', result_dir, '-noPK'], stdout=subprocess.PIPE)
     output = proc.communicate()[0]
     print('Got stdout:', str(output).replace('\\r', '\n').replace('\\n', '\n'))
 
@@ -48,11 +54,13 @@ def run():
     #profiling('/home/slhuang/Public/2018-fall/lineage_inference/case_study/nb_123977/artifacts/', '.csv') #retail/') #nb_123977/artifacts/')
     #pre_clustering('-exact_schema')
     #preserving_ops('/home/slhuang/Public/2018-fall/lineage_inference/case_study/nb_123977/artifacts/', '.csv') #nb_123977/artifacts/') # /repo_user/')
-    result_dir = "./test/"
+    result_dir = "./result"
+    with_pk = False
     clear_files(result_dir)
     profiling('./dataset/', '.csv', result_dir) #retail/') #nb_123977/artifacts/')
-    pre_clustering('-exact_schema', result_dir)  #('-no_pre_cluster')#
-    preserving_ops('./dataset/', '.csv', result_dir) #nb_123977/artifacts/') # /repo_user/')
-    
+    pre_clustering('-no_pre_cluster', result_dir)  #('-no_pre_cluster')# '-exact_schema'
+    #preserving_ops('./dataset/', '.csv', result_dir, with_pk) #nb_123977/artifacts/') # /repo_user/')
+    preserving_ops('./dataset/', '.csv', result_dir, True)
+    preserving_ops('./dataset/', '.csv', result_dir, True, 100)
 if __name__ == '__main__':
     run()
